95574f99c0eef6f1e164bbb1c443c929e56453d5
felicio_lopess <feliciolopesguilherme@gmail.com>
2026-06-19 13:59:02 -0300

    lua1
    lua

---
+++ lib/screens/moon.dart
@@ -0,0 +1,19 @@
+name: fish_phases
+description: "Calendário de Fases da Lua para pesca."
+publish_to: 'none'
+version: 1.0.0
+
+environment:
+  sdk: ^3.8.1
+
+dependencies:
+  flutter:
+    sdk: flutter
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^5.0.0
+
+flutter:
+  uses-material-design: true
